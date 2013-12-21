811705e14367a7cfc4ef458af99572f94a574775
Valdis Geisters <valdis@majaslapa.lv>
2013-12-21 12:14:48 +0200

    Buttons update

--- application/controllers/UploadController.php
+++ application/controllers/UploadController.php
@@ -31,16 +31,10 @@ class UploadController extends Zend_Controller_Action
 
     public function uploadAction()
     {
-        $allowedExts = array("gif", "jpeg", "jpg", "png");
+        $allowedExts = array("gif", "jpeg", "jpg", "png","pdf");
         $temp = explode(".", $_FILES["file"]["name"]);
         $extension = end($temp);
-        if ((($_FILES["file"]["type"] == "image/gif")
-            || ($_FILES["file"]["type"] == "image/jpeg")
-            || ($_FILES["file"]["type"] == "image/jpg")
-            || ($_FILES["file"]["type"] == "image/pjpeg")
-            || ($_FILES["file"]["type"] == "image/x-png")
-            || ($_FILES["file"]["type"] == "image/png"))
-            && ($_FILES["file"]["size"] < 200000000000000)
+        if (($_FILES["file"]["size"] < 200000000000000)
             && in_array($extension, $allowedExts))
         {
             if ($_FILES["file"]["error"] > 0)
